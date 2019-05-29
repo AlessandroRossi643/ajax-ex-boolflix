28eaa5f86aba2a51bd5f8ff0f18ae1cc8180faf1
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-29 11:15:02 +0200

    2. Milestone 2 corretta

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ $(document).ready(function(){
   var template_function=Handlebars.compile(template_source);
 
   var urlBase="https://api.themoviedb.org/3";
+  // var urlBaseImg=
+
+  var bandiereDisponibili=["en","it","es","de","us","fr"];
+  var tipo=""
+  tipo?"film":"serietv";
 
   $('#searchfilm').keydown(function(event){
     if (event.which==13) {
       var filmSerieTVcercato=$('#searchfilm').val();
-      cercaFilm(filmSerieTVcercato,urlBase);
+      cercaScheda(filmSerieTVcercato,urlBase);
 
       $('#searchfilm').val('');
 
@@ -18,7 +23,9 @@ $(document).ready(function(){
     }
   });
 
-  function cercaFilm(ricercaUtente,url){
+
+  // FUNCTION CERCAFILM & SERIETV
+  function cercaScheda(ricercaUtente,url){
     $.ajax({
       url:urlBase+"/search/movie",
       method:"GET",
@@ -30,15 +37,52 @@ $(document).ready(function(){
       success:function(data){
         var filmtrovati=data.results;
         console.log(filmtrovati);
-        stampaFilm(filmtrovati);
+        stampaScheda(filmtrovati);
+      },
+      error: function(){
+        alert("Ops, qualcosa è andato storto");
+      }
+    });
+
+    $.ajax({
+      url:urlBase+"/search/tv",
+      method:"GET",
+      data: {
+        'api_key':"a9cdf17c8817e1396596ef700c4b1a43",
+        'query': ricercaUtente,
+        'language': "it"
+      },
+      success:function(data){
+        var serieTvtrovate=data.results;
+        console.log(serieTvtrovate);
+        var serietvConvertite=convertiChiavi(serieTvtrovate);
+        stampaScheda(serietvConvertite);
       },
       error: function(){
         alert("Ops, qualcosa è andato storto");
       }
     });
   }
-  
-  function stampaFilm(elencofilm){
+
+  //FUNCTION CONVERTI CHIAVI SERIETV
+  function convertiChiavi(serietv){
+    var serieconvertite=[];
+    for (var i = 0; i < serietv.length; i++) {
+      var nuovaSerie={
+        'title': serietv[i].name,
+        'original_title': serietv[i].original_name,
+        'original_language': serietv[i].original_language,
+        'vote_average':serietv[i].vote_average
+      }
+      serieconvertite.push(nuovaSerie);
+    }
+    return serieconvertite;
+  }
+
+
+  // FUNCTION STAMPA SCHEDA FILM O SERIE TV
+  function stampaScheda(elencofilm){
+
     for (var i = 0; i < elencofilm.length; i++) {
       var titoloFilm= elencofilm[i].title;
       var titoloOriginale= elencofilm[i].original_title;
@@ -48,73 +92,99 @@ $(document).ready(function(){
 
       var bandieraLingua=generaBandiera(linguaOriginale);
       var stelle=generaVoto(votoFilm);
+      var html_stelle=creaStelleinHTML(stelle);
 
       var placeholder = {
         'titolo': titoloFilm,
         'titoloOrig': titoloOriginale,
         'lingua': bandieraLingua,
-        'voto': stelle
+        'voto': html_stelle
         }
 
       $('.container_cards').append(template_function(placeholder));
-
     }
   }
 
+  // FUNCTION GENERA BANDIERA
   function generaBandiera(lingua){
-    var bandiera="";
-    switch (lingua) {
-      case "en":
-        bandiera="gb.png";
-        break;
-      case "de":
-        bandiera="de.png";
-        break;
-      case "es":
-        bandiera="es.png";
-        break;
-      case "it":
-        bandiera="it.png";
-        break;
-      case "us":
-        bandiera="us.png";
-        break;
-      case "fr":
-        bandiera="fr.png";
-        break;
-      default:
-        bandiera="ndisp";
-        break;
+    if (bandiereDisponibili.includes(lingua)) {
+      return '<img src="img/'+ lingua +'.png">';
     }
-    return bandiera;
+    else{
+      return lingua;
+    }
   }
 
-
+  // FUNCTION GENERA VOTO
   function generaVoto(voto){
-    votoAvg = Math.round(voto/2);
-    var numeroStelle="";
-    console.log(votoAvg);
-
-    switch (votoAvg) {
-      case 0:
-        numeroStelle= "-";
-        break;
-      case 1:
-        numeroStelle="<i class=\"fas fa-star\"></i>";
-        break;
-      case 2:
-        numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
-        break;
-      case 3:
-        numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
-        break;
-      case 4:
-        numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
-        break;
-      case 5:
-        numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
-        break;
+    return votoAvg=Math.round(voto/2);
+  }
+  //FUNCTION GENERA STELLE IN HTML
+  function creaStelleinHTML(numeroStelle){
+    var icone_stelle="";
+    for (var i = 0; i < 5; i++) {
+      if (i<numeroStelle) {
+        icone_stelle += '<i class="fas fa-star"></i>';
+      }
+      else {
+        icone_stelle += '<i class="far fa-star"></i>';
+      }
     }
-    return numeroStelle;
+    return icone_stelle;
   }
 });
+
+
+//  METODI USATI PRECEDENTEMENTE
+//   function generaVoto(voto){
+//     votoAvg = Math.round(voto/2);
+//     var numeroStelle="";
+//     console.log(votoAvg);
+//
+//     switch (votoAvg) {
+//       case 0:
+//         numeroStelle= "-";
+//         break;
+//       case 1:
+//         numeroStelle="<i class=\"fas fa-star\"></i>";
+//         break;
+//       case 2:
+//         numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
+//         break;
+//       case 3:
+//         numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
+//         break;
+//       case 4:
+//         numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
+//         break;
+//       case 5:
+//         numeroStelle="<i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i><i class=\"fas fa-star\"></i>";
+//         break;
+//     }
+//     return numeroStelle;
+//   }
+
+// function generaBandiera(lingua){
+//   var bandiera="";
+//   switch (lingua) {
+//     case "en":
+//       bandiera="gb.png";
+//       break;
+//     case "de":
+//       bandiera="de.png";
+//       break;
+//     case "es":
+//       bandiera="es.png";
+//       break;
+//     case "it":
+//       bandiera="it.png";
+//       break;
+//     case "us":
+//       bandiera="us.png";
+//       break;
+//     case "fr":
+//       bandiera="fr.png";
+//       break;
+//   }
+//   return bandiera;
+// }
